4833e0b99d1cc9bfc4e1469e023204ad9795565f
Joseph Sabo <jsephsabo@gmail.com>
2025-07-21 17:51:41 -0600

    Arithmetic

---
+++ Arithmetic/src/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Arithmetic Operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        // Addition
+        // z = x + y;
+        // Subtraction
+        // z = x - y;
+        // Multiplication
+        // z = x * y;
+        // Division
+        // z = x / y;
+        // Remainder
+        // z = x % y;
+
+
+        // Augumented Assignment Operators
+        // x += y;
+
+
+        // Increment and Decrement Operatoes
+        // x++;
+
+
+        System.out.println(x);
+
+    }
+}
